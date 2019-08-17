5be60cbeeb9ad071ee69cdd20266a4c1ed53b566
Sven Luijten <svenluijten1995@gmail.com>
2019-08-17 19:57:28 +0200

    Add more functions & tests

--- arrays-slices/sum.go
+++ arrays-slices/sum.go
@@ -1,9 +1,27 @@
 package arraysslices
 
 // Sum adds all the numbers in the given array
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
+	}
+	return
+}
+
+// SumAll maps over the given slice of slices, and returns the sums for each
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+// SumAllTails adds all the tails of each slice passed in
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
 	}
 	return
 }
--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -2,22 +2,55 @@ package arraysslices
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		actual := Sum(numbers)
+		expected := 15
 
-	if want != got {
-		t.Errorf("got %d, want %d. Given %v", got, want, numbers)
+		if expected != actual {
+			t.Errorf("got %d, want %d. Given %v", actual, expected, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	actual := SumAll([]int{1, 2}, []int{0, 9})
+	expected := []int{3, 9}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v want %v", actual, expected)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	actual := SumAllTails([]int{1, 2}, []int{0, 9})
+	expected := []int{2, 9}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v, expected %v", actual, expected)
 	}
 }
 
 func ExampleSum() {
-	result := Sum([5]int{1, 2, 3, 4, 5})
+	result := Sum([]int{1, 2, 3, 4, 5})
 	fmt.Println(result)
 	// Output: 15
+}
+
+func ExampleSumAll() {
+	result := SumAll([]int{1, 2, 3}, []int{4, 5, 6})
+	fmt.Println(result)
+	// Output: [6 15]
+}
+
+func ExampleSumAllTails() {
+	result := SumAllTails([]int{1, 2, 3}, []int{4, 5, 6})
+	fmt.Println(result)
+	// Output: [5 11]
 }
